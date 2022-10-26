069276abc40aecabd7314203d0a767d449cf9a77
sushantkr961 <sushantonly961@gmail.com>
2022-10-26 11:04:35 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-6xl font-bold underline bg-purple-600">
+      <h1 className="text-2xl font-bold underline bg-purple-600">
         Sushant Kumar Portfolio is on the way. You will get details after
         somedays. I'm working on this website.
       </h1>
