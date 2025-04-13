45cc9127b05b9938930ccf9c1e09248cf3a75d1e
Sushant Kumar <sushantonly961@gmail.com>
2025-04-13 13:52:00 +0530

    solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/sushantkr961.github.io',
-  assetPrefix: '/sushantkr961.github.io',
 };
 
 export default nextConfig;
