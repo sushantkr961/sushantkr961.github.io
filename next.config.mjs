7514fe2b650aeffafcc14fe1fa15663e887aab6f
Sushant Kumar <sushantonly961@gmail.com>
2025-04-17 23:00:46 +0530

    refactor: remove generated type files for layout and page
    chore: update next.config.mjs for static export settings
    
    fix: update homepage in package.json to reflect new base path
    
    build: modify deploy script to include export step before deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/sushant_porfolio',
+  assetPrefix: '/sushant_porfolio/',
 };
 
 export default nextConfig;
